cc06ad67783f97a974783ebe2dc6d3f09259fed0
linus krönström <linuskronstrom@live.com>
2020-10-17 15:15:21 +0200

    sorted usings

--- AdventureGame/Item.cs
+++ AdventureGame/Item.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AdventureGame {
+﻿namespace AdventureGame {
     public class Item {
         public string Name { get; set; }
         public string Info { get; set; }
